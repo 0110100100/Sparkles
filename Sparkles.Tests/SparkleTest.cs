2e43c8c8f7e7ea8dd62b2aaa0a20f007489d2423
Octopus <thsctbrkm@gmail.com>
2024-06-14 19:41:47 +0300

    add solution  and tests

---
+++ Sparkles.Tests/SparkleTest.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+
+[TestFixture]
+public class ProgramTests
+{
+    private Program _program;
+
+    [SetUp]
+    public void Setup()
+    {
+        _program = new Program();
+    }
+
+    [Test]
+    [TestCase(10, 5, 28)]
+    [TestCase(1, 2, 2)]
+    [TestCase(10, 2, 20)] 
+    [TestCase(0, 3, 0)]
+    [TestCase(6, 3, 24)]
+    public void CalculateTotalHours_WhenCalled_ReturnsExpectedResult(int c1, int b1, int expectedHours)
+    {
+        int result = _program.CalculateTotalHours(c1, b1);
+
+        Assert.That(result, Is.EqualTo(expectedHours));
+    }
+}
--- Sparkles/Program.cs
+++ Sparkles/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+public class Program
+{   
+    public int CalculateTotalHours(int c1, int b1)
+    {
+        int totalHours = 0;
+        int currentSparklers = c1;
+        int extinguishedSparklers = 0;
+        int previousSparklers = -1; //чтобы программа не уходила в бесконечный цикл, пример с кейсом 10 и 2
 
-Console.WriteLine("Hello, World!");
+        while (currentSparklers > 0 && currentSparklers != previousSparklers)
+        {
+            previousSparklers = currentSparklers;
+            totalHours += 2 * currentSparklers;
+            extinguishedSparklers += currentSparklers;
+
+            int newSparklers = (extinguishedSparklers / b1) * 2;
+            extinguishedSparklers %= b1;
+
+            if (newSparklers == 0) break;
+            currentSparklers = newSparklers;
+        }
+
+        return totalHours;
+    }
+    
+
+    static void Main(string[] args)
+    {
+    }
+}
+
